4eff7878563b8acbbbaff7e501af9ea5b4f544a2
Jesús Héctor López Grajeda <jesushlopezg@gmail.com>
2020-05-24 16:24:27 +0200

    Little program to create, calculate and save the inverse of a matrix. And if possible, retrieve the inverse from the cache.

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <- function(x = matrix()) {
+  im <- NULL
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) im <<- inverse
+  getinverse <- function() im
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  im <- x$getinverse()
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  matrix <- x$get()
+  im <- solve(matrix, ...)
+  x$setinverse(im)
+  im
+}
